16e0e5d00cdbe4775b850c7530d00d9de3380fcb
Smriti Thapa <ismritithapa07@gmail.com>
2026-02-08 20:11:05 +0545

    single blog page

--- blogger_haven/urls.py
+++ blogger_haven/urls.py
@@ -46,12 +46,15 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
+from blogs import views as BlogsView
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('<slug:slug>/', BlogsView.blogs, name='blog'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
