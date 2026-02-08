616df7f1e17bb8e9b604c1d333ee216dc410b492
Smriti Thapa <ismritithapa07@gmail.com>
2026-02-08 20:55:10 +0545

    about us feature

--- blogger_haven/views.py
+++ blogger_haven/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from assignments.models import About
 
 # def home(request):
 #     categories = Category.objects.all()
@@ -15,8 +16,15 @@ def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('-created_at')
     posts = Blog.objects.filter(is_featured=False, status="Published").order_by('-created_at')
 
+    #Feth about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html',  context)
