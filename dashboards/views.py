5cb72fd9f658211afa452c0701723bf4b3fad54d
Smriti Thapa <ismritithapa07@gmail.com>
2026-02-10 16:19:12 +0545

    custom dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()
@@ -13,4 +15,8 @@ def dashboard(request):
         'category_count': category_count,
         'blogs_count': blogs_count,
     }
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard/dashboard.html', context)
+
+
+def categories(request):
+    return render(request, 'dashboard/categories.html')
